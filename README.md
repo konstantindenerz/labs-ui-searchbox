1b55d409a50edda20a0ebbbf5973192cb3fb684e
konstantindenerz <konstantin.denerz+bitbucket@gmail.com>
2016-01-18 01:41:15 -0800

    Add new search box component to demo project.

---
+++ README.md
@@ -0,0 +1,16 @@
+# labs.ui.searchbox
+
+Animated search box component built with Angular 2 (beta).
+
+## Preconditions
+
+```shell
+npm i
+```
+
+
+## Start web server
+
+```shell
+npm start
+```
--- app/searchbox.js
+++ app/searchbox.js
@@ -2,11 +2,30 @@
   app.SearchBox =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
+      templateUrl: 'app/searchbox.html',
+      styleUrls: ['app/searchbox.css']
     })
     .Class({
       constructor: function() {
-        
+        this.vm = {
+          collapsed : true
+        };
+      },
+      open: function(event, inputElement){
+        if(this.vm.collapsed){
+          inputElement.focus();
+          inputElement.value = null;
+          this.vm.collapsed = false;
+          event.stopPropagation();
+        }
+
+      },
+      close: function(event){
+        if(!this.vm.collapsed){
+          this.vm.collapsed = true;
+          event.stopPropagation();
+        }
+
       }
     });
 })(window.app || (window.app = {}));
